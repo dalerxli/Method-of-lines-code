13dcae546bc09d9781281395a99f89701f6cb6da
Aleksy Prokopyszyn <aleksyprok@gmail.com>
2020-06-26 17:10:49 +0100

    a

--- control.py
+++ control.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import solve_bvp
 from scipy.interpolate import interp1d
-import tridiag2 as td2
+from scipy.fft import fft, ifft, fftfreq
 
 def vA(x,z):
 	return vA0 * (1 + x) * (epsilon + (1 - epsilon) * (1 + np.tanh(np.sin(np.pi * z / Lz) / h0)) / 2)
@@ -16,13 +16,11 @@ def rhox(x,z):
 	return -2 * vA0 / (1 + x) ** 3 / (epsilon + (1 - epsilon) * (1 + np.tanh(np.sin(np.pi * z / Lz) / h0)) / 2) ** 2
 
 def ux_ana(x,z):
-	return ux0 * np.log(x - 1j * xi) * sol.sol(z)[0]
+	return -beta0 * np.log(x - 1j * xi) * (1j * k_perp * sol.sol(z)[0] - np.sin(alpha) * sol.sol(z)[1])
 
-# def b_par_ana(x,z):
-# 	return b_par0 * omega ** 2 * rhox(x,z) * sol.sol(z)[0]
 def b_par_ana(x,z0):
-	f = interp1d(z, b_par_x_min)
-	return f(z0)
+	f = interp1d(z, b_par0)
+	return f(z0) + x - x
 
 def u_perp_ana(x,z):
 	return beta0 / (x - 1j * xi) * sol.sol(z)[0]
@@ -42,14 +40,14 @@ def bcs(phi_a, phi_b, p):
 
 Lz = 1
 kz = np.pi / Lz
-nz = 1024
+nz = 4096
 z_min = 0
 z_max =  2 * Lz
 dz = (z_max - z_min) / nz
 z = np.linspace(z_min + dz / 2, z_max - dz / 2, nz)
 
-alpha = 0.0 * np.pi
-k_perp = 0.5 * np.pi
+alpha = 0.1 * np.pi
+k_perp = 0.1 * np.pi
 vA0 = 1
 epsilon = 0.1
 h0 = 0.25
@@ -65,10 +63,10 @@ omega   = omega_r + 1j * omega_i
 
 xi = -2 * omega_i / omega_r * rho(0,0) / rhox(0,0)
 
-nx = 128
-lx = 4 * abs(xi)
+nx = 512
+lx = 2 * abs(xi)
 x_min = -lx
-x_max = lx
+x_max =  lx
 dx = (x_max - x_min) / (nx - 1)
 x = np.linspace(x_min, x_max, nx)
 
@@ -77,12 +75,19 @@ X, Z = np.meshgrid(x, z)
 nabla_perp0 = 1j * k_perp
 
 beta0 = xi
-ux0     = -nabla_perp0 * beta0
-b_par0  = beta0 / (1j * omega * nabla_perp0)
+
+# Calculate b_par0
+L1 = omega ** 2 * rhox(0,z)
+kzn = 2 * np.pi * fftfreq(nz, dz)
+kzn[0] = 1 # To avoid division by zero, note that fft(f)[0] is approximately equal to zero anyway
+f = beta0 * L1 / (1j * omega) * sol.sol(z)[0]
+b_par0 = ifft(fft(f) / 1j / (k_perp - kzn * np.sin(alpha)))
 
 ux_x_min     = ux_ana(x_min, z)
-# b_par_x_min  = b_par_ana(x_min, z)
-b_par_x_min  = td2.calc_b_par0()
+b_par_x_min  = b_par_ana(x_min, z)
 u_perp_x_min = u_perp_ana(x_min, z)
 
 U_x_min = np.concatenate((ux_x_min, b_par_x_min))
+
+nabla_perp_b_par0 = -np.sin(alpha) / (2 * dz) * (np.roll(b_par0, -1) - np.roll(b_par0, 1)) \
+					+ 1j * k_perp * b_par0
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from mpl_toolkits import mplot3d
 from matplotlib import cm
 from scipy.linalg import solve_banded
 from scipy.integrate import solve_ivp
+from scipy.fftpack import diff as psdiff
 import control as ct
 import tridiag as td
+import plots as pt
 
 def dUdx(x, U):
 	ux = U[0:ct.nz]
@@ -22,47 +24,36 @@ def dUdx(x, U):
 sol = solve_ivp(dUdx, [ct.x_min, ct.x_max], ct.U_x_min, t_eval=ct.x, method='RK45', rtol=1e-4, atol=1e-8)
 
 ux = sol.y[0:ct.nz,:]
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(ct.X, ct.Z, np.real(ux), cmap=cm.cool)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(ct.X, ct.Z, np.real(ct.ux_ana(ct.X,ct.Z)), cmap=cm.cool)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
-
 b_par = sol.y[ct.nz:,:]
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(ct.X, ct.Z, np.imag(b_par), cmap=cm.cool)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(ct.X, ct.Z, np.imag(ct.b_par_ana(ct.X,ct.Z)), cmap=cm.cool)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
-
 u_perp = np.zeros((ct.nz, ct.nx), dtype=complex)
 for ix in range(ct.nx):
 	u_perp[:,ix] = td.calc_u_perp(ct.x[ix],b_par[:,ix])
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(ct.X, ct.Z, np.imag(u_perp), cmap=cm.cool)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(ct.X, ct.Z, np.imag(ct.u_perp_ana(ct.X,ct.Z)), cmap=cm.cool)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
+# pt.plot_along_z(ux, ct.nx // 2, r'$u_x(z)$')
+# pt.plot_along_x(ux, ct.nz // 4, r'$u_x(x)$')
+# pt.surface_real(np.real(ux), r'$Re[u_x(x,z)]$')
+# pt.surface_real(np.imag(ux), r'$Im[u_x(x,z)]$')
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(ct.z, np.real(ct.b_par_x_min))
-ax.plot(ct.z, np.real(ct.b_par_ana(ct.x_min, ct.z)))
+# pt.plot_along_z(b_par, ct.nx // 2, r'$b_{||}(z)$')
+# pt.plot_along_x(b_par, ct.nz // 4, r'$b_{||}(x)$')
+# pt.surface_real(np.real(b_par), r'$Re[b_{||}(x,z)]$')
+# pt.surface_real(np.imag(b_par), r'$Im[b_{||}(x,z)]$')
+
+# pt.plot_along_x(u_perp, ct.nz // 4, r'$u_\perp(x)$')
+# pt.surface_real(np.real(u_perp), r'$Re[u_\perp(x,z)]$')
+# pt.surface_real(np.imag(u_perp), r'$Im[u_\perp(x,z)]$')
+
+# pt.ux_ana_vs_num_along_z(ux, ct.nx // 2)
+# pt.ux_ana_vs_num_along_x(ux, ct.nz // 4)
+
+# pt.b_par_ana_vs_num_along_z(b_par, ct.nx // 2)
+# pt.b_par_ana_vs_num_along_x(b_par, ct.nz // 4)
+
+# pt.u_perp_ana_vs_num_along_z(u_perp, ct.nx // 2)
+# pt.u_perp_ana_vs_num_along_x(u_perp, ct.nz // 4)
+
+pt.plot_vA(ct.nx // 2, ct.nz // 4)
+
+# pt.ux_doc(ux, ct.nx // 2, ct.nz // 4)
+# pt.b_par_doc(b_par, ct.nx // 2, ct.nz // 4)
+# pt.u_perp_doc(u_perp, ct.nx // 2, ct.nz // 4)
